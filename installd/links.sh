472f5b6f5df3b7fe3c4011dd17f043c546afb5f9
Yevhenii Havrylko <egavrilko@gmail.com>
2024-07-08 19:17:18 -0700

    Improve links install

--- installd/links.sh
+++ installd/links.sh
@@ -5,7 +5,7 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
 rm ~/.zshrc
 ln -s $BASEDIR/zshrc ~/.zshrc
 mkdir -p ~/.zshrcd/init
-cat $BASEDIR/zshrcd/conda.sh > ~/.zshrcd/conda.sh
+ln -s $BASEDIR/zshrcd/conda.sh > ~/.zshrcd/conda.sh
 ln -s $BASEDIR/zshrcd/wsl.sh ~/.zshrcd/wsl.sh
 ln -s $BASEDIR/zshrcd/zoxide.sh ~/.zshrcd/zoxide.sh
 ln -s $BASEDIR/zshrcd/init/_settings.sh ~/.zshrcd/init/_settings.sh
@@ -16,4 +16,6 @@ ln -s $BASEDIR/p10k.zsh ~/.p10k.zsh
 ln -s $BASEDIR/tmux.conf ~/.tmux.conf 
 
 ln -s $BASEDIR/gitconfig ~/.gitconfig
+
+cat $BASEDIR/condarc > ~/.condarc
 
